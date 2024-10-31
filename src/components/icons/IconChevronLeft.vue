cbed726f403e75202f2302be9c22aeb8f5713f1d
Maciej Romanowski <neipreis22@gmail.com>
2024-10-31 18:03:47 +0100

    added icons

---
+++ src/components/icons/IconChevronLeft.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :height="height"
+    viewBox="0 -960 960 960"
+    :width="width"
+    :fill="color"
+  >
+    <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z"/>
+  </svg>
+</template>
+
+<script setup lang="ts">
+export interface Props {
+  width?: number;
+  height?: number;
+  color?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  width: 24,
+  height: 24,
+  color: 'currentColor',
+});
+</script>
---
+++ src/components/icons/IconChevronRight.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 -960 960 960"
+    :height="height"
+    :width="width"
+    :fill="color"
+  >
+    <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/>
+  </svg>
+</template>
+
+<script setup lang="ts">
+export interface Props {
+  width?: number;
+  height?: number;
+  color?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  width: 24,
+  height: 24,
+  color: 'currentColor',
+});
+</script>
